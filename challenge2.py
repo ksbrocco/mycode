7aa47c34fa7c516cf19a43f162e6da3fa10129a6
Kevin Sbrocco <ksbrocco@gmail.com>
2019-10-14 16:30:32 -0400

    Day 1

---
+++ challenge2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+col_list= ["blue", "Columbus"]
+col_list.append(1492)
+name = input("What is your name? ")
+print(f"In {col_list[2]}, {col_list[1]} sailed the ocean {col_list[0]}. {name} fell off the boat.")
+
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_input = input("Please enter an IPv4 IP address: ")
+vendor_name = input("What is the name of the vendor for this device? ")
+print("You told me the IPv4 address is:", user_input)
+print("The vendor for this device is:", vendor_name)
+
